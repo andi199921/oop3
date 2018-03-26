baa948da4d3439d842e56a504e30a1d336e579db
Перевалов Андрей <eczio@live.com>
2018-03-26 10:52:00 +0300

    Initial commit

---
+++ oop3/Fraction.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace oop3
+{
+    class FractionWork
+    {
+        int m, n, Nod, p, Q;
+        public int Enter()
+        {
+            Console.WriteLine("Введите первый числитель");
+            m = int.Parse(Console.ReadLine());
+            return m;
+            Console.WriteLine("Введите первый знаменатель");
+            n = int.Parse(Console.ReadLine());
+            return n;
+            Console.WriteLine("Введите второй числитель");
+            p = int.Parse(Console.ReadLine());
+            return p;
+            Console.WriteLine("Введите второй знаменатель");
+            Q = int.Parse(Console.ReadLine());
+            return Q;
+            
+        }
+
+        public int Reduction()
+        {
+            Enter();
+            n = int.Parse(Console.ReadLine());
+            while (Math.Abs(m)!=Math.Abs(n))
+            {
+                if (m>n)
+                {
+                    m -= n;
+                }
+                else n -= m;
+            }
+            Nod = m;
+            return m;
+
+        }
+        public void Spar()
+        {
+            int a = p;
+            int b = Q;
+            p = p / Reduction();
+            Q = Q / Reduction();
+            Console.WriteLine("Исходная дробь {0}/{1}, сокращённая {2}/{3}",a,b,p,Q);
+        }
+        public int Multiplication()
+        {
+            Enter();
+            int a = m * p;
+            int b = n * Q;
+            Console.WriteLine("Дробь {0}/{1} умножим на {2}/{3} равна {4}/{5}", m,n,p,Q,a,b);
+            return a / b;
+        }
+        public int Division()
+        {
+            Enter();
+            int a = m * Q;
+            int b = n * p;
+            Console.WriteLine("Дробь {0}/{1} разделим на {2}/{3} равна {4}/{5}", m, n, p, Q, a, b);
+            return a / b;
+        }
+    }
+}
---
+++ oop3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace oop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FractionWork fraction = new FractionWork();
+            do
+            {
+                Console.WriteLine("Действия\n1 - Сокращение\n2 - Умножение\n3 - Деление");
+                string line = Console.ReadLine();
+                if (CheckInt(line) && (int.Parse(line) > 0 && int.Parse(line) < 3))
+                {
+                    switch (line)
+                    {
+                        case "1":
+                            {
+                                Console.WriteLine(fraction.Reduction());
+                                break;
+                            }
+                        case "2":
+                            {
+                                Console.WriteLine(fraction.Multiplication());
+                                break;
+
+                            }
+                        case "3":
+                            {
+                                Console.WriteLine(fraction.Division());
+                                break;
+                            }
+                        default: break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Такого варианта нет\n");
+                }
+            } while (true);
+        }
+        private static bool CheckInt(string number)
+        {
+            try
+            {
+                int.Parse(number);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static int EnterInt(string text)
+        {
+            Console.WriteLine(text);
+            do
+            {
+                string line = Console.ReadLine();
+                if (CheckInt(line))
+                {
+                    return int.Parse(line);
+                }
+                else { Console.WriteLine("Это не число"); }
+            } while (true);
+        }
+    }
+}
+    
+
